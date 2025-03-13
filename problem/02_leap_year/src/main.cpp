ada36ceaa9940a71821bd6041bb5919f8046ee6b
elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-13 20:33:37 +0200

    updated main file leap year

--- problem/02_leap_year/src/main.cpp
+++ problem/02_leap_year/src/main.cpp
@@ -1,19 +1,10 @@
 #include <iostream>
-#include "InputValidation.hpp"
-#include "InputString.hpp"
-#include "utils.hpp"
+#include "date.hpp"
 #include "enums.hpp"
 
 int main()
 {
-    std::string year = InputString::readYear();
-
-    while (!InputValidation::isValidYear(year))
-    {
-        Utils::clearInputError();
-        std::cout << "Enter a valid year (between 1000 and 9999): " << std::endl;
-        year = InputString::readYear();
-    }
+    std::string year = Date::readYear();
 
     if (Enums::isLeapYear(std::stoi(year)) == LeapYearType::LEAP_YEAR)
         std::cout << year << " is a leap year." << std::endl;
