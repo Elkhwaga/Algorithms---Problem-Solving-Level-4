394460e47035ecb027b4067dbb81d2ac41464159
elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-17 01:52:05 +0200

    create problem increase date by one week

---
+++ problem/20_increase_date_by_one_week/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "date.hpp"
+#include "InputNumber.hpp"
+
+int main()
+{
+    MyDate date = date.readFullDate();
+    date = date.increaseDateByOneWeek(date);
+
+    std::cout << "\nAfter adding one week is: "
+              << date.day << "/" << date.month << "/" << date.year << std::endl;
+
+    return 0;
+}
