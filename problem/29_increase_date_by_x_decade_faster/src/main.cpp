34561c018e5466197b39350d0352d7c0ddd3adf1
elkhwaga <mohamedelkhwaga9@gmail.com>
2025-03-17 04:37:14 +0200

    create problem increase date by x decades faster

--- problem/29_increase_date_by_x_decade_faster/src/main.cpp
+++ problem/29_increase_date_by_x_decade_faster/src/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     MyDate date = date.readFullDate();
     unsigned short decades = InputNumber::readNumberInRange("Enter a number of decades: ", 1, 1000);
-    date = date.increaseDateByXDecade(decades, date);
+    date = date.increaseDateByXDecadesFaster(decades, date);
 
     std::cout << "\nAdding " << decades << " decades is: "
               << date.day << "/" << date.month << "/" << date.year << std::endl;
